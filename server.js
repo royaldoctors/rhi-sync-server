1505ce67fa6eb162b41cde59b8028cd2c8046c1a
royaldoctors <royalherbs9ja@gmail.com>
2026-04-10 07:40:55 +0100

    server.js
    const express = require('express');
    const cors = require('cors');
    
    const app = express();
    
    app.use(cors()); // VERY IMPORTANT
    app.use(express.json());
    
    let storedData = {};
    
    // POST
    app.post('/rhi-sync', (req, res) => {
        storedData = req.body;
        res.json({ status: 'saved' });
    });
    
    // GET
    app.get('/rhi-sync', (req, res) => {
        res.set('Cache-Control', 'no-store');
        res.json(storedData);
    });
    
    // Test
    app.get('/', (req, res) => {
        res.send('RHI Sync Server is running');
    });
    
    const PORT = process.env.PORT || 3000;
    app.listen(PORT, () => console.log('Server running'));

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors()); // VERY IMPORTANT
 app.use(express.json());
 
 let storedData = {};
 
-// POST - Save data
+// POST
 app.post('/rhi-sync', (req, res) => {
     storedData = req.body;
     res.json({ status: 'saved' });
 });
 
-// GET - Fetch data
+// GET
 app.get('/rhi-sync', (req, res) => {
     res.set('Cache-Control', 'no-store');
     res.json(storedData);
 });
 
-// Test route (important)
+// Test
 app.get('/', (req, res) => {
     res.send('RHI Sync Server is running');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server running on port ' + PORT));
+app.listen(PORT, () => console.log('Server running'));
